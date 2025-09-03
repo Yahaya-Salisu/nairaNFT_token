caf9c2610c2d60b5780c129baf8a1b6713d70b70
Yahaya Salisu <yahayasalisu162@gmail.com>
2025-09-03 09:40:58 +0100

    Update test and script folders

--- contracts/script/deployNairaNFT.s.sol
+++ contracts/script/deployNairaNFT.s.sol
@@ -1,1 +1,17 @@
-1
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import { Script } from "forge-std/Script.sol";
+import "../src/nairaNFT.sol";
+
+contract deployNairaNFT is Script {
+    nairaNFT public NFT;
+
+    function run() public {
+
+        vm.startBroadcast();
+        NFT = new nairaNFT();
+        vm.stopBroadcast();
+    }
+}
--- contracts/test/nairaNFT.t.sol
+++ contracts/test/nairaNFT.t.sol
@@ -1,1 +1,20 @@
-1
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import { Test } from "forge-std/Test.sol";
+import "../src/nairaNFT.sol";
+
+contract nairaNFT is nairaNFT, Test {
+    nairaNFT public NFT;
+    address public owner;
+    address public User1;
+    address public User2;
+
+    function setUp() public {
+        NFT = new nairaNFT();
+        owner = address(this);
+        User1 = makeAddr("User1");
+        User2 = makeAddr("User2");
+    }
+}
